065de075abdff7526d707c87cd611bad3818a270
marshall <mhamad.marshall@gmail.com>
2022-10-17 14:29:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if ( ride >= 2501) {
+    return 'No can do.';
+     } if ( ride >= 2001) {
+     return `I will gladly take your thirty bucks.`;
+     }if (ride <= 400 ) {
+     return `This one is on me!`;
+     } else if ( 400 >= ride <= 2000 ) {
+     return `That will be twenty bucks.`;
+     }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city == 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+  case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.'
+  case 'thanks for everything':
+     return 'Bye.' }
 }
